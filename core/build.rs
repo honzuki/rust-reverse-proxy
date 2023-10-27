d71fec6f67692ac8fd91a3ab0fd9aa083e824d52
honzuki <22032642+honzuki@users.noreply.github.com>
2023-10-27 17:50:56 +0300

    add support to old protoc compilers

--- core/build.rs
+++ core/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .include_file("generated_proto.rs")
         .compile(&["proto/reverse-proxy.proto"], &["/proto"])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
